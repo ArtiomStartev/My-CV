a6215e600a77e7a6cf210cd10a39ddc2155fc47c
Artiom Startev <96334648+ArtiomStartev@users.noreply.github.com>
2022-12-11 10:34:22 +0200

    added send email lambda function

---
+++ sendEmailLambdaFunction.js
@@ -0,0 +1,47 @@
+var AWS = require("aws-sdk");
+
+const responseHeaders = { "Access-Control-Allow-Origin": "*" };
+const ses = new AWS.SES({ region: "us-east-1" });
+const toAddress = "artiomstartsev2003@gmail.com";
+const subject = "New Email from website user Artiom Startev CV";
+
+exports.handler = async (event) => {
+  const body = JSON.parse(event.body);
+
+  const data = `User Name : ${body.userName}\n`.concat(`Email : ${body.email}\n`).concat(`Message : ${body.message}\n`);
+
+  const params = {
+    Destination: { ToAddresses: [toAddress] },
+    Message: { Body: { Text: { Data: data } }, Subject: { Data: subject } },
+    Source: toAddress,
+  };
+
+  let mailSendingError = false;
+  await sendMail(params)
+    .then((data, error) => {
+      if (error) {
+        console.log("Error : ", error);
+        mailSendingError = true;
+      }
+      console.log("data : ", data);
+    })
+    .catch((error) => {
+      console.log("Error : ", error);
+      mailSendingError = true;
+    });
+
+  if (mailSendingError) {
+    return { statusCode: 201, headers: responseHeaders, body: JSON.stringify({ success: false }) };
+  } else {
+    return { statusCode: 201, headers: responseHeaders, body: JSON.stringify({ success: true }) };
+  }
+};
+
+const sendMail = async (params) => {
+  return await new Promise((resolve, reject) => {
+    ses.sendEmail(params, (error, data) => {
+      if (error) reject(error);
+      else resolve(data);
+    });
+  });
+};
